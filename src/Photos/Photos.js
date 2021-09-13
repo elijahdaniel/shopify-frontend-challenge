bf6f77a24e209e97349a0f4ab51da9a13f0621ae
elijahdaniel <e.paminsan@gmail.com>
2021-09-13 16:12:48 -0700

    added id to images and changed borders around cards

--- src/Photos/Photos.js
+++ src/Photos/Photos.js
@@ -7,24 +7,29 @@ function Photos({ data }) {
         return (
           <div className={style.photoCard} key={mrp.id}>
             <img src={mrp.img_src} alt={mrp.rover.name} />
+            <div className={style.itemNum}>{mrp.id}</div>
             <section className={style.cardDetails}>
-              <h3>Camera</h3>
-              <p>
-                <b>Name:</b> {mrp.camera.full_name} ({mrp.camera.name})
-              </p>
-              <h3>Rover</h3>
-              <p>
-                <b>Name:</b> {mrp.rover.name}
-              </p>
-              <p>
-                <b>Landing Date:</b> {mrp.rover.landing_date}
-              </p>
-              <p>
-                <b>Launch Date:</b> {mrp.rover.launch_date}
-              </p>
-              <p>
-                <b>Current Status:</b> {mrp.rover.status}
-              </p>
+              <section className={style.rover}>
+                <h3>Rover</h3>
+                <p>
+                  <b>Name:</b> {mrp.rover.name}
+                </p>
+                <p>
+                  <b>Landing Date:</b> {mrp.rover.landing_date}
+                </p>
+                <p>
+                  <b>Launch Date:</b> {mrp.rover.launch_date}
+                </p>
+                <p>
+                  <b>Current Status:</b> {mrp.rover.status}
+                </p>
+              </section>
+              <section className='camera'>
+                <h3>Camera</h3>
+                <p>
+                  <b>Name:</b> {mrp.camera.full_name} ({mrp.camera.name})
+                </p>
+              </section>
             </section>
           </div>
         );
